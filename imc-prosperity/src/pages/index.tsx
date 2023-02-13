feccf0a56666e0c93ceb2826c07fd1435673cdf4
Luke Prior <22492406+Uskompuf@users.noreply.github.com>
2023-02-13 21:19:02 +1100

    initial

---
+++ imc-prosperity/src/pages/index.tsx
@@ -0,0 +1,78 @@
+import Head from 'next/head'
+import Button from '@mui/material/Button';
+import { ChangeEvent, useState } from "react";
+import { parse } from "csv-parse/browser/esm/sync";
+
+type performanceLog = {
+  day: number;
+  timestamp: number;
+  product: string;
+  bid_price_1: number;
+  bid_volume_1: number;
+  bid_price_2?: number | null;
+  bid_volume_2?: number | null;
+  bid_price_3?: number | null;
+  bid_volume_3?: number | null;
+  ask_price_1: number;
+  ask_volume_1: number;
+  ask_price_2?: number | null;
+  ask_volume_2?: number | null;
+  ask_price_3?: number | null;
+  ask_volume_3?: number | null;
+  mid_price: number;
+  profit_and_loss: number;
+};
+
+export default function Home() {
+  const [csvData, setCsvData] = useState<performanceLog[]>([]);
+  const [filename, setFilename] = useState("");
+
+  const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+
+    const file = e.target.files[0];
+    const { name } = file;
+    setFilename(name);
+
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      if (!evt?.target?.result) {
+        return;
+      }
+      const { result } = evt.target;
+      const records = parse(result as string, {
+        columns: ["day", "timestamp", "product", "bid_price_1", "bid_volume_1", "bid_price_2", "bid_volume_2", "bid_price_3", "bid_volume_3", "ask_price_1", "ask_volume_1", "ask_price_2", "ask_volume_2", "ask_price_3", "ask_volume_3", "mid_price", "profit_and_loss"],
+        delimiter: ";",
+        from_line: 2,
+        trim: true,
+        skip_empty_lines: true
+      });
+      setCsvData(records);
+    };
+    reader.readAsBinaryString(file);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>IMC Prosperity Dashboard</title>
+        <meta name="description" content="IMC Prosperity Dashboard" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>
+          <h1>IMC Prosperity Dashboard</h1>
+          <p>{filename}</p>
+          <p>{JSON.stringify(csvData)}</p>
+          <Button variant="contained" component="label">
+            Upload
+            <input hidden accept="text/csv" type="file" onChange={handleFileUpload} />
+          </Button>
+        </div>
+      </main>
+    </>
+  )
+}
